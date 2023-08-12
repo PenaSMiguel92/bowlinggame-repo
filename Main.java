2ecc8c0af6fc0f2b6410d5b6e4faafe7e06a0d74
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-12 14:31:28 -0700

    fixed case where 10 is at first index

--- Main.java
+++ Main.java
@@ -10,9 +10,12 @@ public class Main {
         int[] player1_game3 = new int[] { 2, 3 };
         int[] player2_game3 = new int[] { 4, 1 };
 
+        int[] player1_game1193 = new int[] { 10, 2, 2, 3 };
+        int[] player2_game1193 = new int[] { 3, 8, 4, 5 };
 
         System.out.println(sol.isWinner(player1_game1, player2_game1));
         System.out.println(sol.isWinner(player1_game2, player2_game2));
         System.out.println(sol.isWinner(player1_game3, player2_game3));
+        System.out.println(sol.isWinner(player1_game1193, player2_game1193));
     }
 }
--- Solution.java
+++ Solution.java
@@ -1,6 +1,5 @@
 public class Solution {
     public int isWinner(int[] player1, int[] player2) {
-        int value = 0;
         int p1_score = 0;
         int p2_score = 0;
         for (int index = 0; index < player1.length; index++) {
@@ -16,8 +15,10 @@ public class Solution {
     }
 
     public int calculateScore(int[] player, int currentIndex) {
-        if (currentIndex - 1 < 0 || currentIndex - 2 < 0)
-            return player[currentIndex];
+        if (currentIndex - 1 < 0) return player[currentIndex];
+        if (currentIndex - 2 < 0)
+            return (player[currentIndex - 1] == 10) ? 2 * player[currentIndex] : player[currentIndex];
+        
         return (player[currentIndex - 1] == 10 || player[currentIndex - 2] == 10) ? 2 * player[currentIndex]
                 : player[currentIndex];
     }
