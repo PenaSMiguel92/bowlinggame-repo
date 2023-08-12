894e059b17b674f00f3fcd09bc54348b4a2747d0
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-12 14:06:53 -0700

    main code complete

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Solution sol = new Solution();
+        int[] player1_game1 = new int[] { 4, 10, 7, 9 };
+        int[] player2_game1 = new int[] { 6, 5, 2, 3 };
+
+        int[] player1_game2 = new int[] { 3, 5, 7, 6 };
+        int[] player2_game2 = new int[] { 8, 10, 10, 2 };
+
+        int[] player1_game3 = new int[] { 2, 3 };
+        int[] player2_game3 = new int[] { 4, 1 };
+
+
+        System.out.println(sol.isWinner(player1_game1, player2_game1));
+        System.out.println(sol.isWinner(player1_game2, player2_game2));
+        System.out.println(sol.isWinner(player1_game3, player2_game3));
     }
 }
