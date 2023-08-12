1f0a740164313c415bffa44a27a06013e58ad008
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-12 14:21:58 -0700

    fixed issue with wrong value being returned

--- Solution.java
+++ Solution.java
@@ -7,7 +7,12 @@ public class Solution {
             p1_score += calculateScore(player1, index);
             p2_score += calculateScore(player2, index);
         }
-        return value;
+        if (p1_score > p2_score)
+            return 1;
+        if (p2_score > p1_score)
+            return 2;
+        
+        return 0;
     }
 
     public int calculateScore(int[] player, int currentIndex) {
